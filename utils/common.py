72758ba2f734203435415ac3f43096a19b5eb33e
asingh86 <asingh86@gmail.com>
2021-08-08 18:38:23 +0100

    updating some filepaths

--- utils/common.py
+++ utils/common.py
@@ -6,6 +6,6 @@ def read_configs() -> dict:
     This functions read config file
     :return: dict
     """
-    with open('/app/configs/configs.yml', 'r') as stream:
+    with open('./configs/configs.yml', 'r') as stream:
         config = yaml.load(stream, Loader=yaml.SafeLoader)
     return config
